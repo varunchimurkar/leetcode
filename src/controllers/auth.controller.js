d04fa5407ff806f814327e906c1c75fc89a714db
varunchimurkar <varunchimurkar96@gmail.com>
2025-05-02 22:44:51 +0530

    Logout Controller ✅ - User logout Successfully

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -44,6 +44,7 @@ export const register = async (req, res) => {
         })
 
         res.status(201).json({
+            success:true,
             message: "User Created Successfully",
             user: {
                 id: newUser.id,
@@ -100,6 +101,7 @@ export const login = async (req, res) => {
         })
 
         res.status(200).json({
+            success:true,
             message: "User Logged in Successfully",
             user: {
                 id: user.id,
@@ -119,7 +121,25 @@ export const login = async (req, res) => {
 }
 
 export const logout = async (req, res) => {
+    try {
+        res.clearCookie("jwt", {
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV != "development",
 
+        })
+
+        res.status(200).json({
+            success: true,
+            message: "User logged out successfully"
+        })
+
+    } catch (error) {
+        console.error("Error creating user:", error)
+        res.status(500).json({
+            error: "Error logging out user"
+        })
+    }
 }
 
 export const check = async (req, res) => {
