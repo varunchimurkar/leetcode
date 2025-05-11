ab6f7c8fddde83c140766d0f3a5f112286c53270
varunchimurkar <varunchimurkar96@gmail.com>
2025-05-11 09:12:30 +0530

    Add Functionality - getAllProblemsId & updateProblem Controller ✨

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -37,9 +37,9 @@ export const createProblem = async (req, res) => {
 
             for (let i = 0; i < results.length; i++) {
 
-                const result = results[i]
+               const result = results[i]
 
-                console.log("Result -- ----", results)
+              //  console.log("Result -- ----", results)
 
                 if (result.status.id !== 3) {
                     return res.status(400).json({ error: ` Testcase ${i + 1} failed for language ${language}` })
@@ -70,7 +70,7 @@ export const createProblem = async (req, res) => {
 
 
     } catch (error) {
-        console.log(error)
+       // console.log(error)
         return res.status(500).json({
             error: "Error While Creating Problem"
         })
@@ -79,24 +79,24 @@ export const createProblem = async (req, res) => {
 }
 
 export const getAllProblems = async (req, res) => {
-      
-    try{
+
+    try {
 
         const problems = await db.problem.findMany()
 
-        if(!problems){
+        if (!problems) {
             return res.status(404).json({
-                error:"No problems found"
+                error: "No problems found"
             })
         }
 
         res.status(200).json({
-            success:true,
-            message:"Message Fetched Successfully",
+            success: true,
+            message: "Message Fetched Successfully",
             problems
         })
 
-    } catch(error) {
+    } catch (error) {
         console.log(error)
         return res.status(500).json({
             error: "Error While Fetching Problem"
@@ -108,13 +108,129 @@ export const getAllProblems = async (req, res) => {
 
 export const getProblemById = async (req, res) => {
 
-    
+    const { id } = req.params
 
-    
+    try {
+
+        const problem = await db.problem.findUnique({
+            where: {
+                id
+            }
+        })
+
+        if (!problem) {
+            return res.status(404).json({
+                error: "Problem not found"
+
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Message Created Successfully",
+            problem
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            error: "Error While Fetching Problem by id"
+        })
+    }
+
+
 }
 
 export const updateProblem = async (req, res) => {
 
+    const { id } = req.params
+
+    const { title, description, difficulty, tags, userId, constraints,
+        codeSnippets, referenceSolutions, examples, testcases } = req.body
+
+    if (req.user.role !== "ADMIN") {
+        return res.status(403).json({
+            error: "You are not allowed to update a problem"
+        })
+    }
+    try {
+
+        const existingproblem = await db.problem.findUnique({
+            where: {
+                id
+            }
+        })
+
+        if (!existingproblem) {
+            return res.status(404).json({
+                error: "Problem not found"
+
+            })
+        }
+
+
+        for (const [language, solutioncode] of Object.entries(referenceSolutions)) {
+            const languageId = getJudge0LanguageId(language)
+
+            if (!languageId) {
+                return res.status(400).json({
+                    error: `Language ${language} is not supported`
+                })
+            }
+
+            const submissions = testcases.map(({ input, output }) => ({
+                source_code: solutioncode,
+                language_id: languageId,
+                stdin: input,
+                expected_output: output
+            }))
+
+            const submissionsResults = await submitBatch(submissions)
+
+            const tokens = submissionsResults.map((res) => res.token)
+
+            const results = await pollBatchResults(tokens)
+
+            for (let i = 0; i < results.length; i++) {
+
+                const result = results[i]
+
+               // console.log("Result -- ----", results)
+
+                if (result.status.id !== 3) {
+                    return res.status(400).json({ error: ` Testcase ${i + 1} failed for language ${language}` })
+                }
+            }
+
+            const UpdatedProblem = await db.problem.update({
+                where : { id },
+                data: {
+                    title,
+                    description,
+                    difficulty,
+                    tags,
+                    constraints,
+                    codeSnippets,
+                    referenceSolutions,
+                    examples,
+                    testcases
+                    
+                }
+            })
+
+            return res.status(201).json({
+                success: true,
+                message: "Problem updated Successfully",
+                problem: UpdatedProblem
+            })
+        }
+
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            error: "Error While updating Problem"
+        })
+    }
 }
 
 export const deleteproblem = async (req, res) => {
