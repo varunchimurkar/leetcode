35312590e434b950ddc46b62d34a1c360a7e214b
varunchimurkar <varunchimurkar96@gmail.com>
2025-05-11 08:30:47 +0530

    Add Functionality - getAllProblems Controller

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,99 +1,126 @@
-import {db} from "../libs/db.js"
+import { db } from "../libs/db.js"
 import { getJudge0LanguageId, submitBatch } from "../libs/judge0.lib.js"
 import { pollBatchResults } from "../libs/judge0.lib.js"
 
 
-export const createProblem = async(req, res) => {
-  const {title,  description, difficulty,  tags, userId, constraints, 
-    codeSnippets, referenceSolutions, examples, testcases} = req.body
+export const createProblem = async (req, res) => {
+    const { title, description, difficulty, tags, userId, constraints,
+        codeSnippets, referenceSolutions, examples, testcases } = req.body
 
-if(req.user.role !== "ADMIN"){
-    return res.status(403).json({
-        error : "You are not allowed to create a problem" 
-    })
-}
-  try {
-     for(const [language, solutioncode] of Object.entries(referenceSolutions)){
-        const languageId = getJudge0LanguageId(language)
+    if (req.user.role !== "ADMIN") {
+        return res.status(403).json({
+            error: "You are not allowed to create a problem"
+        })
+    }
+    try {
+        for (const [language, solutioncode] of Object.entries(referenceSolutions)) {
+            const languageId = getJudge0LanguageId(language)
 
-        if(!languageId) {
-            return res.status(400).json({
-                error : `Language ${language} is not supported`
+            if (!languageId) {
+                return res.status(400).json({
+                    error: `Language ${language} is not supported`
+                })
+            }
+
+            const submissions = testcases.map(({ input, output }) => ({
+                source_code: solutioncode,
+                language_id: languageId,
+                stdin: input,
+                expected_output: output
+            }))
+
+            const submissionsResults = await submitBatch(submissions)
+
+            const tokens = submissionsResults.map((res) => res.token)
+
+            const results = await pollBatchResults(tokens)
+
+            for (let i = 0; i < results.length; i++) {
+
+                const result = results[i]
+
+                console.log("Result -- ----", results)
+
+                if (result.status.id !== 3) {
+                    return res.status(400).json({ error: ` Testcase ${i + 1} failed for language ${language}` })
+                }
+            }
+
+            const newProblem = await db.problem.create({
+                data: {
+                    title,
+                    description,
+                    difficulty,
+                    tags,
+                    constraints,
+                    codeSnippets,
+                    referenceSolutions,
+                    examples,
+                    testcases,
+                    userId: req.user.id
+                }
+            })
+
+            return res.status(201).json({
+                success: true,
+                message: "Message Created Successfully",
+                problem: newProblem
             })
         }
 
-        const submissions = testcases.map(({input, output}) => ({
-            source_code: solutioncode,
-            language_id: languageId,
-            stdin:input,
-            expected_output:output
-        }))
 
-        const submissionsResults = await submitBatch(submissions)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            error: "Error While Creating Problem"
+        })
+    }
 
-        const tokens = submissionsResults.map((res) => res.token)
-         
-        const results = await pollBatchResults(tokens)
+}
 
-        for(let i=0; i < results.length; i++){
-           
-            const result = results[i]
+export const getAllProblems = async (req, res) => {
+      
+    try{
 
-            console.log("Result -- ----", results)
+        const problems = await db.problem.findMany()
 
-            if(result.status.id !==3) {
-                return res.status(400).json({error :` Testcase ${i+1} failed for language ${language}`})
-            }
+        if(!problems){
+            return res.status(404).json({
+                error:"No problems found"
+            })
         }
 
-        const newProblem = await db.problem.create({
-             data: {
-                title,  
-                description, 
-                difficulty,  
-                tags, 
-                constraints, 
-                codeSnippets, 
-                referenceSolutions, 
-                examples, 
-                testcases,
-                userId : req.user.id
-             }
+        res.status(200).json({
+            success:true,
+            message:"Message Fetched Successfully",
+            problems
         })
 
-        return res.status(201).json({
-            success: true,
-            message: "Message Created Successfully",
-            problem:newProblem
+    } catch(error) {
+        console.log(error)
+        return res.status(500).json({
+            error: "Error While Fetching Problem"
         })
     }
 
 
-  } catch(error) {
-      console.log(error)
-      return res.status(500).json({
-        error:"Error While Creating Problem"
-      })
-  }
-
 }
 
-export const getAllProblems = async(req, res) => {
+export const getProblemById = async (req, res) => {
 
-}
+    
 
-export const getProblemById = async(req, res) => {
     
 }
 
-export const updateProblem = async(req, res) => {
-    
+export const updateProblem = async (req, res) => {
+
 }
 
-export const deleteproblem = async(req, res) => {
-    
+export const deleteproblem = async (req, res) => {
+
 }
 
-export const getAllProblemsSolvedByUser = async(req, res) => {
-    
+export const getAllProblemsSolvedByUser = async (req, res) => {
+
 }
