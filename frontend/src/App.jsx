211e550fc9ee396a1dc9b6bfb9fb0a5241667baa
Varun Chimurkar <varunchimurkar0908@gmail.com>
2025-06-02 22:39:04 +0530

    Create -> useAuthStore.jsx & add -> functionality, install axios, react-hot-toast, zod, zustand

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-import {Routes, Route, Navigate} from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
+import { Toaster } from "react-hot-toast"
+
 import SignUpPage from './page/SignUpPage'
 import HomePage from './page/HomePage'
 import LoginPage from './page/LoginPage'
+import { Loader } from 'lucide-react'
 
 const App = () => {
 
-  let authUser = null 
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
+
+  //let authUser = null
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <Loader className="size-10 animate-spin" />
+      </div>
+    )
+  }
 
   return (
     <div className='flex flex-col items-center justify-start'>
- <Routes>
+      <Toaster />
+      <Routes>
 
-  <Route
-  path = '/'
-  element = {authUser ? <HomePage/> : <Navigate to={"/login"}/>}
-  
-  />
-  <Route
-  path = '/login'
-  element = {!authUser ? <LoginPage/> : <Navigate to={"/"} />}
-/>
+        <Route
+          path='/'
+          element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
 
-  <Route
-  path = '/signup'
-  element = {!authUser ? <SignUpPage/> : <Navigate to={"/"} />}
-/>
- </Routes>
+        />
+        <Route
+          path='/login'
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+
+        <Route
+          path='/signup'
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
+      </Routes>
 
     </div>
   )
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,104 @@
+import { create } from "zustand"
+import { axiosInstance } from "../lib/axios"
+
+import toast from "react-hot-toast"
+
+
+export const userAuthStore = create((set) => ({
+    authUser: null,
+    isSigninUp: false,
+    isLoggingIn: false,
+    isCheckingAuth: false,
+
+
+    checkAuth: async () => {
+
+        set({ isCheckingAuth: true })
+
+        try {
+            const res = await axiosInstance.get("/auth/check")
+            console.log("checkauth res", res.data)
+            set({ authUser: res.data.user })
+
+        } catch (error) {
+            console.log("❌ Error checking auth:", error)
+            set({ authUser: null })
+        }
+        finally {
+            set({ isCheckingAuth: false })
+        }
+
+    },
+
+
+    signup: async (data) => {
+
+        set({ isSigninUp: true })
+
+        try {
+            const res = await axiosInstance.get("/auth/register", data)
+
+            console.log("signup respose", res.data)
+
+            set({ authUser: res.data.user })
+
+            toast.success(res.data.message)
+
+        } catch (error) {
+            console.log("❌ Error signing up:", error)
+            //set({authUser:null})
+            toast.error("Error signing up")
+        }
+        finally {
+            set({ isSigninUp: false })
+        }
+
+    },
+
+    login: async (data) => {
+
+        set({ isLoggingIn: true })
+
+        try {
+            const res = await axiosInstance.get("/auth/login", data)
+
+            console.log("logging respose", res.data)
+
+            set({ authUser: res.data.user })
+
+            toast.success(res.data.message)
+
+        } catch (error) {
+            console.log("❌ Error Logging in:", error)
+            //set({authUser:null})
+            toast.error("Error Logging in")
+        }
+        finally {
+            set({ isLoggingIn: false })
+        }
+
+    },
+
+    logout: async () => {
+
+
+
+        try {
+            const res = await axiosInstance.get("/auth/logout")
+
+            console.log("logout respose", res.data)
+
+            set({ authUser: res.data.user })
+
+            toast.success(res.data.message)
+
+        } catch (error) {
+            console.log("❌ Error logging out:", error)
+            //set({authUser:null})
+            toast.error("Error logging out")
+        }
+
+
+    }
+}))
+
