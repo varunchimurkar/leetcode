22719bcea10150a61fdd757db770e02b61c88c75
Varun Chimurkar <varunchimurkar0908@gmail.com>
2025-08-28 11:47:47 +0530

    Modify -> Axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios"
 export const axiosInstance = axios.create({
     baseURL : import.meta.env.MODE === "development" 
     ? "http://localhost:8080/api/v1" 
-    : "/api/v1",
+    : import.meta.env.VITE_API_BASE_URL + "/api/v1",
     withCredentials:true
 })
