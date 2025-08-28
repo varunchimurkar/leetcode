5722df75b2208a1ca08ce31355fdfd90fb83b080
Varun Chimurkar <varunchimurkar0908@gmail.com>
2025-08-28 20:34:21 +0530

    production not working - for testing purpose

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,5 +4,6 @@ export const axiosInstance = axios.create({
     baseURL : import.meta.env.MODE === "development" 
     ? "http://localhost:8080/api/v1" 
     : import.meta.env.VITE_API_BASE_URL + "/api/v1",
-    withCredentials:true
+    withCredentials:true,
+     headers: { "Content-Type": "application/json" }
 })
