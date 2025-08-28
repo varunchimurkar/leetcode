2dd960e734edcee5ced50d6e644a2051a7169a83
Varun Chimurkar <varunchimurkar0908@gmail.com>
2025-08-28 18:33:44 +0530

    Prisma@Client re-install

--- backend/src/generated/prisma/edge.js
+++ backend/src/generated/prisma/edge.js
@@ -266,7 +266,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors ({
-   //origin:"http://localhost:5173",
+   // origin:"http://localhost:5173",
     origin:"https://leetcode-lime.vercel.app",
     credentials:true
 }))
